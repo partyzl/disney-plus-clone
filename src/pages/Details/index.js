cb428fd92345fe94229319e99ae105f7d58d30cb
Parth Patel <parth6796@yahoo.com>
2021-09-16 14:00:15 +0100

    added filler text and images and details page

---
+++ src/pages/Details/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Details() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Details
+
+const div = styled.div`
+
+`
