59bdab5e2dfb5e25807043b3578db5e7e78d0215
Parth Patel <parth6796@yahoo.com>
2021-09-16 12:10:09 +0100

    navbar alinks added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -32,6 +32,7 @@ function Header() {
                         <span>SERIES</span>
                     </a>
                 </NavMenu>
+                <UserImg src="https://i.imgur.com/tdi3NGa.jpg"/>
             </Nav>
         </div>
     )
@@ -49,17 +50,22 @@ const Nav = styled.nav`
 
 const Logo = styled.img`
     width: 80px;
+    cursor: pointer;
 
 `
 
 const NavMenu = styled.div`
     display: flex;
     color: white;
+    flex: 1;
+    margin-left: 20px;
+    align-items:center;
 
     a{
         display: flex;
         align-items: center;
         padding: 0 12px;
+        text-decoration: none;
 
         img {
             height: 20px;
@@ -71,6 +77,34 @@ const NavMenu = styled.div`
             text-decoration: none;
             font-size: 13px;
             letter-spacing: 1.42px;
+            position:relative;
+
+            &:after {
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity:0;
+                transform-origin: left center;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
+        }
+        &:hover{
+            span:after{
+                transform: scaleX(1);
+                opacity: 1;
+            }
         }
     }
+`
+
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
 `
