807fee6820189f665c64bb8f4b52e4a2d624509b
Parth Patel <parth6796@yahoo.com>
2021-09-16 13:09:24 +0100

    styling slider

--- src/components/ImgSlider/index.js
+++ src/components/ImgSlider/index.js
@@ -21,10 +21,13 @@ function ImgSlider() {
                 <img src="/images/slider-badging.jpg" alt="slider badging" />
             </Wrap>
             <Wrap>
-                <img src="/images/slider-badging.jpg" alt="slider badging" />
+                <img src="/images/slider-badag.jpg" alt="slider badging" />
             </Wrap>
             <Wrap>
-                <img src="/images/slider-badag.jpg" alt="slider badging" />
+                <img src="/images/slider-scale.jpg" alt="slider badging" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-scales.jpg" alt="slider badging" />
             </Wrap>
         </Carousel>
     )
@@ -36,12 +39,35 @@ export default ImgSlider;
 const Carousel = styled(Slider)`
     margin-top: 20px;
 
+    ul li button {
+        &:before{
+            font-size: 10px;
+            color: rgb(150, 158 171);
+        }
+    }
+
+    li.slick-active button::before {
+        color: white;
+    }
+    }
+
+    .slick-list {
+        overflow: visible;
+    }
+
+    button {
+        z-index: 1;
+    }
 `
 
 const Wrap = styled.div`
     img{
+        border: 4px solid transparent;
+        border-radius: 4px;
         width: 100%;
         height: 100%;
+        box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
+        rgb(0 0 0 /73%) 0px 16px 10px -10px;
 
     }
 `
