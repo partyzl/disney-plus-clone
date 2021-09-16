60d9c5fa3e766ead5ea832022ffbaa7fd243e084
Parth Patel <parth6796@yahoo.com>
2021-09-16 11:37:16 +0100

    added icons to navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,30 @@ function Header() {
             <Nav>
                 <Logo src= "/images/logo.svg" />
                 <NavMenu>
-
+                    <a href="hi">
+                        <img src="/images/home-icon.svg" alt="" />
+                        <span>HOME</span>
+                    </a>
+                    <a href="hi">
+                        <img src="/images/search-icon.svg" alt="" />
+                        <span>SEARCH</span>
+                    </a>
+                    <a href="hi">
+                        <img src="/images/watchlist-icon.svg" alt="" />
+                        <span>WATCHLIST</span>
+                    </a>
+                    <a href="hi">
+                        <img src="/images/original-icon.svg" alt="" />
+                        <span>ORIGINALS</span>
+                    </a>
+                    <a href="hi">
+                        <img src="/images/movie-icon.svg" alt="" />
+                        <span>MOVIES</span>
+                    </a>
+                    <a href="hi">
+                        <img src="/images/series-icon.svg" alt="" />
+                        <span>SERIES</span>
+                    </a>
                 </NavMenu>
             </Nav>
         </div>
@@ -30,5 +53,24 @@ const Logo = styled.img`
 `
 
 const NavMenu = styled.div`
+    display: flex;
+    color: white;
 
+    a{
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+
+        }
+
+        span {
+            color: white;
+            text-decoration: none;
+            font-size: 13px;
+            letter-spacing: 1.42px;
+        }
+    }
 `
