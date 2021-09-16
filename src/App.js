d98ff13524fdee618c90e764141d07b578e91d28
Parth Patel <parth6796@yahoo.com>
2021-09-16 14:01:26 +0100

    added imports for detail page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Header} from './components'
-import {Home} from './pages'
+import {Home, Details} from './pages'
 
 function App() {
   return (
     <div className="App">
       < Header />
       <Home />
+      <Details />
     </div>
   );
 }
