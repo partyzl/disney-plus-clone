1f5a79b2dafbad4d39311a20162abc57e18722d0
Parth Patel <parth6796@yahoo.com>
2021-09-16 11:03:01 +0100

    adding Header component and imported to App.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <div>
+            <Container>
+            Header
+            </Container>
+        </div>
+    )
+}
+
+export default Header
+
+const Container = styled.div`
+    height: 70px;
+    background: #090b13
+`
+
