36bd07ccf112543f9d1b43d5af6a50a021e8c39a
Parth Patel <parth6796@yahoo.com>
2021-09-16 12:57:26 +0100

    added extra images in carousel

--- src/components/ImgSlider/index.js
+++ src/components/ImgSlider/index.js
@@ -1,17 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
 import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function ImgSlider() {
 
-    
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    }
 
     return (
-        <div>
-            
-        </div>
+        <Carousel {...settings}>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" alt="slider badging" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" alt="slider badging" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badag.jpg" alt="slider badging" />
+            </Wrap>
+        </Carousel>
     )
 }
 
 export default ImgSlider;
+
+
+const Carousel = styled(Slider)`
+    margin-top: 20px;
+
+`
+
+const Wrap = styled.div`
+    img{
+        width: 100%;
+        height: 100%;
+
+    }
+`
