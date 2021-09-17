62be4486e27f8d01bb39eda34adee2a8d8ea8834
Parth Patel <parth6796@yahoo.com>
2021-09-17 09:37:03 +0100

    added links to movie descriptions from cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import './App.css';
-import {Header} from './components'
-import {Home, Details, Login} from './pages'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Header } from './components';
+import { Home, Details, Login } from './pages';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        < Header />
-        <Switch>
-          <Route exact path="/"><Home/></Route>
-          <Route path="/details"><Details/></Route>
-          <Route path="/login"><Login/></Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/details/:id">
+						<Details />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
